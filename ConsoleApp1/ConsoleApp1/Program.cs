e068f2fb5c6f92655c03517c57f0a4bff159e587
kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-08 13:37:59 +0100

    Dodanie metody GetAverage

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,18 @@ Console.WriteLine("Hello, Frytki !");
 
 Console.WriteLine("Pora na smażenie");
 Console.WriteLine("Modyfikacja 3 nie mam pomysłu");
+
+
+static double GetAverage(int [] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+    return (double)sum / numbers.Length;
+}
+
+/*int[] tablica = { 1, 2, 3, 4, 5 };
+double average = GetAverage(tablica);
+Console.WriteLine(average);*/
